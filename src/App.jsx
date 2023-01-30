95d63e1bcbdb02b4c42e9e03c218dca9bb11dabb
Cazier92 <bryce.cazier@gmail.com>
2023-01-29 20:15:59 -0800

    add wallet to navbar, stub up handleExchange in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,25 @@ import './styles/App.css'
 import Nav from './Components/Nav/Nav'
 import { useState } from 'react'
 
-
 // Components
 import Landing from './Components/Landing/Landing'
 import BurgerShop from './Components/BurgerShop/BurgerShop'
+import SuperMarket from './Components/SuperMarket/SuperMarket'
 
 const App = () => {
   const [daytime, setDaytime] = useState(true)
+  const [cash, setCash] = useState(100)
+  const handleExchange = (amt) => {
+    setCash()
+  }
   return (
     <>
-    <Nav setDaytime={setDaytime}/>
+    <Nav setDaytime={setDaytime} cash={cash}/>
     <main>
       <Routes>
         <Route path='/' element={<Landing daytime={daytime}/>}/>
         <Route path='/burgers' element={<BurgerShop />}/>
+        <Route path='/market' element={<SuperMarket />} setCash={setCash}/>
       </Routes>
     </main>
     </>
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -2,12 +2,15 @@
 import Logo from '../../assets/react-logo.png'
 import { NavLink } from 'react-router-dom'
 import Clock from './Clock'
+import Wallet from './Wallet'
 
-const Nav = ({setDaytime}) => {
+const Nav = ({setDaytime, cash}) => {
   return (
     <nav className="navigation-bar">
       <NavLink to='/' id='logo'><img src={Logo} alt="" /></NavLink>
       <NavLink to='/burgers'>BURGER SHOP</NavLink>
+      <NavLink to='/market'>SUPER MARKET</NavLink>
+      <Wallet cash={cash}/>
       <Clock setDaytime={setDaytime}/>
     </nav>
   )
